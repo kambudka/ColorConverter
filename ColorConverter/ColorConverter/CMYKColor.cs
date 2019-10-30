d025f519bf56c4c67feb148254f82434386b05c0
kbudk <kbudk@BLK2512>
2019-10-30 11:45:00 +0100

    Cube + Converter

---
+++ ColorConverter/ColorConverter/CMYKColor.cs
@@ -0,0 +1,44 @@
+﻿namespace ColorConverter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class CmykColor
+    {
+        private readonly float _c;
+
+        private readonly float _m;
+
+        private readonly float _y;
+
+        private readonly float _k;
+
+        public CmykColor(float c, float m, float y, float k)
+        {
+            _c = c;
+            _m = m;
+            _y = y;
+            _k = k;
+        }
+
+        public float C
+        {
+            get { return _c; }
+        }
+
+        public float M
+        {
+            get { return _m; }
+        }
+
+        public float Y
+        {
+            get { return _y; }
+        }
+
+        public float K
+        {
+            get { return _k; }
+        }
+    }
+}
---
+++ ColorConverter/ColorConverter/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColorConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ColorPicker1_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            R.Value = ColorPicker1.SelectedColor.Value.R;
+            G.Value = ColorPicker1.SelectedColor.Value.G;
+            B.Value = ColorPicker1.SelectedColor.Value.B;
+
+            CmykColor color = ConvertRgbToCmyk(ColorPicker1.SelectedColor.Value.R, 
+                                                ColorPicker1.SelectedColor.Value.G,
+                                                ColorPicker1.SelectedColor.Value.B);
+            C.Value = (double)color.C *100;
+            M.Value = (double)color.M * 100;
+            Y.Value = (double)color.Y * 100;
+            K.Value = (double)color.K * 100;
+
+        }
+
+        private void ChangeToCMYK_Click(object sender, RoutedEventArgs e)
+        {
+            CmykColor color = ConvertRgbToCmyk(R.Value.GetValueOrDefault(),
+                                    G.Value.GetValueOrDefault(),
+                                    B.Value.GetValueOrDefault());
+            C.Value = (double)color.C * 100;
+            M.Value = (double)color.M * 100;
+            Y.Value = (double)color.Y * 100;
+            K.Value = (double)color.K * 100;
+        }
+
+        private void ChangeToRGB_Click(object sender, RoutedEventArgs e)
+        {
+            Color color = ConvertCmykToRgb(C.Value.GetValueOrDefault(),
+                                            M.Value.GetValueOrDefault(),
+                                            Y.Value.GetValueOrDefault(),
+                                            K.Value.GetValueOrDefault());
+
+            R.Value = color.R;
+            G.Value = color.G;
+            B.Value = color.B;
+        }
+
+        public static ColorConverter.CmykColor ConvertRgbToCmyk(int r, int g, int b)
+        {
+            float c;
+            float m;
+            float y;
+            float k;
+            float rf;
+            float gf;
+            float bf;
+
+            rf = r / 255F;
+            gf = g / 255F;
+            bf = b / 255F;
+
+            k = ClampCmyk(1 - Math.Max(Math.Max(rf, gf), bf));
+            c = ClampCmyk((1 - rf - k) / (1 - k));
+            m = ClampCmyk((1 - gf - k) / (1 - k));
+            y = ClampCmyk((1 - bf - k) / (1 - k));
+
+            return new ColorConverter.CmykColor(c, m, y, k);
+        }
+
+        public static Color ConvertCmykToRgb(double c, double m, double y, double k)
+        {
+            byte r;
+            byte g;
+            byte b;
+
+            r = Convert.ToByte(255 * (1 - c) * (1 - k));
+            g = Convert.ToByte(255 * (1 - m) * (1 - k));
+            b = Convert.ToByte(255 * (1 - y) * (1 - k));
+
+            return Color.FromRgb(r, g, b);
+        }
+
+        private static float ClampCmyk(float value)
+        {
+            if (value < 0 || float.IsNaN(value))
+            {
+                value = 0;
+            }
+
+            return value;
+        }
+    }
+}
